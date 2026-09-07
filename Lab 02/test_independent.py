41cbff9de9869759c4824e3d75ce9fad7ed65617
6705142017-CMN <6705142017@siam.edu>
2026-09-07 22:20:54 +0700

    Add files via upload

---
+++ Lab 02/test_independent.py
@@ -0,0 +1,13 @@
+from bank import BankAccount
+
+
+def test_deposit_independent():
+    account = BankAccount(100)  # Fresh account
+    account.deposit(50)
+    assert account.balance == 150
+
+
+def test_withdraw_independent():
+    account = BankAccount(100)  # Fresh account
+    account.withdraw(30)
+    assert account.balance == 70
